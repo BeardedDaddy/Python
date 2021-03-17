05d41f0b712a50a3119ee9564d6f9303eeb2804e
Grevy Marcelin <grevyjr@gmail.com>
2021-03-17 17:50:34 -0400

    Relearned how to do Functions

--- Functions.py
+++ Functions.py
@@ -1,9 +1,19 @@
+#A function is a block of code which only runs when it is called. You can pass data, known as paraments, into a function. A function can return data as a result.
+#Example: def my_function(FirstName, LastName):
+#           print(FirstName + "" + LastName)
+#         my_function ("Grevy", "Marcelin")
 
-def check_age(age):
-  if age < 18:
-    print('oops not an adult')
-  else:
-    print('hooray I am an adult')  
+def my_function(FirstName, LastName):
+      print(FirstName + "" + LastName)
+my_function ("Grevy", "Marcelin")
 
-check_age(18)
-check_age(17)
+# To call the function you have to break out of the print statement (indent)
+
+#def check_age(age):
+#  if age < 18:
+#    print('oops not an adult')
+#  else:
+#    print('hooray I am an adult')  
+
+#check_age(18)
+#check_age(17)

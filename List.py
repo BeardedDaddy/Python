ca7e809445dd413e782800f980d44398e8dd4f63
Grevy Marcelin <grevyjr@gmail.com>
2021-03-16 20:54:04 -0400

    Learning Averages in Python

--- List.py
+++ List.py
@@ -4,4 +4,22 @@ print(cars)
 print(cars[0])
 print(cars[1])
 print(cars[2])
-print(cars[3])
+
+a =[3, 10, -1]
+print(a)
+a.append(1)
+print(a)
+a.append('hello')
+print(a)
+a.append([1, 2])
+print(a)
+a.pop()
+print(a)
+a[0] = 100
+print(a)
+b = ('banana', 'apple', 'microsoft')
+print(b)
+temp = b[0]
+b[0] = b[2]
+b[2] = temp
+print(b)

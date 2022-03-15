2d0ad6f4af161ebc0eda5fb70477b3011515e329
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-03-15 05:27:28 -0400

    Fixing the issue with running colorama in command terminal.

--- BeardedDaddy/UdemyClasses/Functions_Intro/colour_print.py
+++ BeardedDaddy/UdemyClasses/Functions_Intro/colour_print.py
@@ -1,5 +1,6 @@
+import colorama
 
-# Some ANSI escape sequences for colours and effects.
+# Some ANSI escape sequences for colours and effects
 BLACK = '\u001b[30m'
 RED = '\u001b[31m'
 GREEN = '\u001b[32m'
@@ -17,15 +18,17 @@ REVERSE = '\u001b[7m'
 
 def colour_print(text: str, effect: str) -> None:
     """
-    Print `text` using the ANSI sequences to change colour, etc.
-    
+    Print `text` using the ANSI sequences to change colour, etc
+
     :param text: The text to print.
-    :param effect: The effect we want. One of the constants
+    :param effect: The effect we want.  One of the constants
         defined at the start of this module.
     """
     output_string = "{0}{1}{2}".format(effect, text, RESET)
     print(output_string)
-    
+
+
+colorama.init()
 colour_print("Hello, Red", RED)
 # test that the colour was reset
 print("This should be in the default terminal colour")
@@ -35,3 +38,4 @@ colour_print("Hello, Bold", BOLD)
 colour_print("Hello, Underline", UNDERLINE)
 colour_print("Hello, Reverse", REVERSE)
 colour_print("Hello, Black", BLACK)
+colorama.deinit()

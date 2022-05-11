46f99a7b16156de4af5a249fb0651a520ced13a0
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-05-11 17:52:23 -0400

    Getting an better understanding of collections.deque

--- BeardedDaddy/Python Test Page.py
+++ BeardedDaddy/Python Test Page.py
@@ -7,47 +7,44 @@ graph = {
     6: [None]
 }
 
+import collections
 from collections import deque
-deque(['a', 'b', 'c'])
+d = deque(['a', 'b', 'c'])
 
-deque('abc')
+# d = deque('abc')
 
-deque([{'data': 'a'}, {'data': 'b'}])
+# d = deque([{'data': 'a'}, {'data': 'b'}])
 
 # Linked list to be iterated.
-#llist = deque("abcde")
-#llist
+llist = deque("abcde")
 
 # Add "f" to the end of the list.
-#llist.append("f")
-#llist
+llist.append("f")
 
 # Remove the element at the end of the list.
 #llist.pop()
 
 # Add the letter "z" at the beginning of the list.
 #llist.appendleft("z")
-#llist
 
 # Remove the element at the beginning of the list.
 #llist.popleft()
-#llist
 
 queue = deque()
 
 queue.append("Mary")
-queue.append("John")
-queue.append("Susan")
+# queue.append("John")
+# queue.append("Susan")
 
-queue.popleft()
-queue.popleft()
+# queue.popleft()
+# queue.popleft()
 
-history = deque()
+# history = deque()
 
-history.appendleft("https://realpython.com/")
-history.appendleft("https://realpython.com/pandas-read-write-files/")
-history.appendleft("https://realpython.com/python-csv/")
+# history.appendleft("https://realpython.com/")
+# history.appendleft("https://realpython.com/pandas-read-write-files/")
+# history.appendleft("https://realpython.com/python-csv/")
 
-history.popleft()
-history.popleft()
-print(history)
+# history.popleft()
+# history.popleft()
+print(queue)

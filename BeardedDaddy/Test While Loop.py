ed446ff231c9458ee117092e8e7e0d652a4b0cde
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-05-11 05:14:05 -0400

    Trying to fix python interpreter

--- BeardedDaddy/Test While Loop.py
+++ BeardedDaddy/Test While Loop.py
@@ -1,61 +1,18 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
 
-    def __repr__(self):
-        return self.data
-
-
-class LinkedList:
+class SlinkedList:
     def __init__(self):
-        self.head = None
-
-    def __repr__(self):
-        node = self.head
-        nodes = []
-        while node is not None:
-            nodes.append(node.data)
-            node = node.next
-        nodes.append("None")
-        return " -> ".join(nodes)
+        self.headval = None
 
 
-llist = LinkedList()
+list1 = SlinkedList()
+list1.headval = Node("Mon")  # type: ignore
+e2 = Node("Tue")
+e3 = Node("Wed")
+#Link first Node to second node
+list1.headval.nextval = end2
 
-first_node = Node("a")
-llist.head = first_node
-
-
-second_node = Node("b")
-third_node = Node("c")
-first_node.next = second_node
-second_node.next = third_node
-
-
-
-def __init__(self, nodes=None):
-    self.head = None
-    if nodes is not None:
-        node = Node(data=nodes.pop(0))
-        self.head = node
-        for elem in nodes:
-            node.next = Node(data=elem)
-            node = node.next
-
-
-def __iter__(self):
-    node = self.head
-    while node is not None:
-        yield node
-        node = node.next
-
-
-llist = LinkedList(["a", "b", "c", "d", "e"])
-
-for i in range(llist):
-    print(llist)
-
-for node in llist:
-    print (node)
 

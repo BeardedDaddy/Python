615dbc70b3e017df15182b6f4dbeaab8a2f3ea30
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-06-08 02:34:49 -0400

    Finished Coding Exercise 18

--- BeardedDaddy/UdemyClasses/Factorial_Functions.py
+++ BeardedDaddy/UdemyClasses/Factorial_Functions.py
@@ -1,3 +1,4 @@
+
 # The factorial of a number is the product of all the values from 1 to the number, inclusive.
 # Thus 4 factorial, which is written as 4!, is calculated as
 # 1*2*3*4=24
@@ -5,10 +6,10 @@
 # 1*2*3*4*5=120
 # The convention is that 0!=1 (not zero, as you might expect)
 
-# Write a function called factorial, that will return the factorial of the number passed as its argument.
+#Write a function called factorial, that will return the factorial of the number passed as its argument.
 # You must include a Docstring, and function annotations, in your function.
 
-# Use a for loop to call your factorial function, to print out the first 36 factorials (0 to 35). Your results should be:
+#Use a for loop to call your factorial function, to print out the first 36 factorials (0 to 35). Your results should be:
 # 0 1
 # 1 1
 # 2 2
@@ -17,17 +18,17 @@
 # 5 120 .....
 
 
-def fact(n):
+def factorial(n: int) -> int:
+    """Return n! (0! is 1)."""
+    if n <= 1:
+        return 1
 
-    f = 1
+    result = 2
+    for n in range(3, n + 1):
+        result *= n
 
-    for i in range(1,n+1):
-        f = f*i
-
-    return f
-x = 5
-
-result = fact(x)
+    return result
 
 
-print(result)
+for i in range(36):
+    print(i, factorial(i))

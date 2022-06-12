f4f2b20f9fc1140cdc13f50cca119be04d9b8fa8
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-06-11 21:50:56 -0400

    I fixed the issue with Exercise 18

--- BeardedDaddy/UdemyClasses/Factorial_Functions.py
+++ BeardedDaddy/UdemyClasses/Factorial_Functions.py
@@ -6,10 +6,10 @@
 # 1*2*3*4*5=120
 # The convention is that 0!=1 (not zero, as you might expect)
 
-#Write a function called factorial, that will return the factorial of the number passed as its argument.
+# Write a function called factorial, that will return the factorial of the number passed as its argument.
 # You must include a Docstring, and function annotations, in your function.
 
-#Use a for loop to call your factorial function, to print out the first 36 factorials (0 to 35). Your results should be:
+# Use a for loop to call your factorial function, to print out the first 36 factorials (0 to 35). Your results should be:
 # 0 1
 # 1 1
 # 2 2
@@ -18,7 +18,7 @@
 # 5 120 .....
 
 
-def my_factorial(n: int) -> int:
+def factorial(n: int) -> int:
     """Return n! (0! is 1)."""
     if n <= 1:
         return 1
@@ -31,4 +31,4 @@ def my_factorial(n: int) -> int:
 
 
 for i in range(36):
-    print(i, my_factorial(i))
+    print(i, factorial(i))

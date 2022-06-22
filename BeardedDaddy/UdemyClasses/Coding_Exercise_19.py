3109e572e7227d819ab7c517dcb622ebf516de2d
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-06-22 05:07:55 -0400

    Working on understanding *args

--- BeardedDaddy/UdemyClasses/Coding_Exercise_19.py
+++ BeardedDaddy/UdemyClasses/Coding_Exercise_19.py
@@ -1,16 +1,17 @@
 # Write a fuction to calculate the sum of all numbers passed as its arguments.
 def sum_numbers(*args: int) -> int:
+    a = sum([2, 3, 4, 5, 6])
+    for num in args:
+        a += num
 
 
-a = sum([2, 3, 4, 5, 6])
 # The numbers in the square bracket are the parameters that are going to be passed as the argument.
-print(a)
+print(sum)
 # Call the function sum_numbers.
 # It should define a single variable argument (a star argument) that will get the values to sum.
 
 # This function must contain a Docstring.
 
-# The parameters must be annotated. 
+# The parameters must be annotated.
 
 # The return value must be annotated.
-

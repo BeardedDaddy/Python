277b1cdb25eec097a17fa93e3afa2417b57509ea
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-07-20 05:24:02 -0400

    Working on parsing data

--- BeardedDaddy/UdemyClasses/hello.py
+++ BeardedDaddy/UdemyClasses/hello.py
@@ -1,2 +1,14 @@
-name = input("Enter your name : ")
-print("Hello",name)
+a = sum([2, 3, 4, 5, 6])
+print(a)
+
+b = sum([1, 2, 3])
+print(b)
+
+c = sum([8, 20, 2])
+print(c)
+
+d = sum([12.5, 3.147, 98.1])
+print(d)
+
+e = sum([1.1, 2.2, 5.5])
+print(e)

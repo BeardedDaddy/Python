69a4ddc938a5861b0132b57e48796f744aa2b527
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-07-21 04:06:29 -0400

    Parsing data

--- BeardedDaddy/UdemyClasses/countries.py
+++
@@ -1,6 +0,0 @@
-file = open(
-    r'C:\Users\grevy\OneDrive\Desktop\Learning Python\BeardedDaddy\UdemyClasses\country_info.py')
-
-    for row in file:
-        data = row.split('|')
-        print(data)

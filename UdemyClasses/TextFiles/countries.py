3f6a45dced7c3b6cb25925bc011a71530c8f61bb
Grevy <grevyjr@gmail.com>
2022-09-29 17:19:54 -0400

    Lesson # 252 of working with text files.

--- UdemyClasses/TextFiles/countries.py
+++ UdemyClasses/TextFiles/countries.py
@@ -1,9 +1,27 @@
 """In this code block we are parsing data in a text file."""
 INPUT_FILENAME = 'country_info.txt'
-
-with open(INPUT_FILENAME, encoding = 'utf-8') as country_file:
+countries = {}
+# This is an empty dictionary.
+with open(INPUT_FILENAME, encoding='utf-8') as country_file:
     for row in country_file:
         data = row.strip('\n').split('|')
         country, capital, code, code3, dialing, timezone, currency = data
-        print(country, capital, code, code3, dialing, timezone, currency, sep='\n\t')
-        
+       # print(country, capital, code, code3, dialing, timezone, currency, sep='\n\t')
+        country_dict = {
+            'name': country,
+            'capital': capital,
+            'country_code': country,
+            'cc3': code3,
+            'dialing_code': dialing,
+            'timezone': timezone,
+            'currency': currency,
+        }
+        # print(country_dict)
+        # countries[country.casefold()] = country_dict
+
+print(countries)
+
+chosen_country = input("Please enter the name of a country: ").casefold()
+if chosen_country in countries:
+    country_data = countries[chosen_country]
+    print(country_data['capital'])

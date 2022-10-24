b89e2b431583f022e119c06c1c526f9b619d49ce
Grevy <grevyjr@gmail.com>
2022-10-24 05:45:00 -0400

    Learning about the record_invoice function.

---
+++ Python_Examples/dictionaries.py
@@ -0,0 +1,8 @@
+"""Dictionaries are data types that can be indexed by keys, which can be any immutable type: strings and numbers.
+"""
+
+# It is best to think of a dictionary as a set of key: value pairs, with the requirement that the keys are unique (within one dictionary). A pair of braces creates an empty dictionary: {}. Placing a comma-separated list of key:value pairs within the braces adds initial key:value pairs to the dictionary; this is also the way dictionaries are written on output.
+
+tel = {'grevy': 5500, 'mia': 3300}
+tel['brittany'] = 4400
+tel['grevy']

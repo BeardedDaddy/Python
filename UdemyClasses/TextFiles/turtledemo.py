a00f51ffc8c8a3d22a06998724fe3b219b8a2bf2
Grevy <grevyjr@gmail.com>
2022-11-11 09:13:49 -0500

    Learning about modules and imports.

---
+++ UdemyClasses/TextFiles/turtledemo.py
@@ -0,0 +1,14 @@
+""" Learning about modules and imports.
+"""
+from turtle import *
+
+DONE = "Well done, you have finished your drawing"
+
+# no inspection PyUnresolvedReferences
+forward(150)
+right(250)
+forward(150)
+circle(75)
+
+done()
+print(DONE)

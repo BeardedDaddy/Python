3590a774aa1dc25907358ff616d66d6851ed9d04
Grevy <grevyjr@gmail.com>
2022-11-11 22:38:44 -0500

    Learning about web browser modules.

--- UdemyClasses/TextFiles/browser.py
+++ UdemyClasses/TextFiles/browser.py
@@ -2,6 +2,8 @@
 """
 import webbrowser
 
-webbrowser.open("https://www.python.org/")
+# webbrowser.open("https://www.python.org/", new=1)
 
-help:(webbrowser)  # type: ignore
+# # help:(webbrowser)  # type: ignore
+# chrome = webbrowser.get(using='chrome')
+# chrome.open_new("https://www.python.org/")

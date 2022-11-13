fe39339c28045938aeba63bf754dcac9bd443a3c
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-11-12 22:35:36 -0500

    Done learning about time zones.

--- UdemyClasses/TextFiles/dateandtime.py
+++ UdemyClasses/TextFiles/dateandtime.py
@@ -10,7 +10,7 @@
 # if time.daylight != 0:
 #     print("\tDaylignt Saving Time is in effect for this location")
 #     print("\tThe DST timezone is " + time.tzname[1])
-    
+
 # print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 # print("UTC time  is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
 

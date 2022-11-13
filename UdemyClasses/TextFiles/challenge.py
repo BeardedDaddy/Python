a0c4bc62d684be152acb8e77bcb80e699cda393d
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-11-12 20:36:03 -0500

    Using time zones.

--- UdemyClasses/TextFiles/challenge.py
+++ UdemyClasses/TextFiles/challenge.py
@@ -4,3 +4,10 @@
 #
 # Use the documentation fo the get_clock_info() function
 # to work out how to call it for each of the clocks.
+
+import time
+
+print("time():\t", time.get_clock_info('time'))
+print("perf_counter():\t", time.get_clock_info('perf_counter'))
+print("monotonic():\t", time.get_clock_info('monotonic'))
+print("process_time():\t", time.get_clock_info('process_time'))

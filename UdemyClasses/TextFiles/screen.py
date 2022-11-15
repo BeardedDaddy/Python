c2c1ee518c8f01a7aac43bc1e92fcbff46bcb406
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-11-15 05:26:33 -0500

    Start of tkinter challenge.

--- UdemyClasses/TextFiles/screen.py
+++ UdemyClasses/TextFiles/screen.py
@@ -3,22 +3,22 @@
 import os
 import tkinter
 
-tkinter._test()
 
+tkinter._test()
 
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Grid Demo")
 mainWindow.geometry('640x480+8+200')
-
+mainWindow['padx'] = 8
 label = tkinter.Label(mainWindow, text='Tkinter Grid Demo')
 label.grid(row=0, column=0, columnspan=3)
 
-mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(0, weight=100)
 mainWindow.columnconfigure(1, weight=1)
-mainWindow.columnconfigure(2, weight=3)
-mainWindow.columnconfigure(3, weight=3)
-mainWindow.columnconfigure(4, weight=3)
+mainWindow.columnconfigure(2, weight=1000)
+mainWindow.columnconfigure(3, weight=600)
+mainWindow.columnconfigure(4, weight=1000)
 mainWindow.rowconfigure(0, weight=1)
 mainWindow.rowconfigure(1, weight=10)
 mainWindow.rowconfigure(2, weight=1)
@@ -41,7 +41,7 @@ optionFrame = tkinter.LabelFrame(mainWindow, text='File Details')
 optionFrame.grid(row=1, column=2, sticky='ne')
 
 rbValue = tkinter.IntVar()
-rbValue.set(3)
+rbValue.set(1)
 radio1 = tkinter.Radiobutton(
     optionFrame, text="Filename", value=1, variable=rbValue)
 radio2 = tkinter.Radiobutton(
@@ -74,17 +74,29 @@ timeFrame['padx'] = 36
 
 # Frame for the date spinners.
 dateFrame = tkinter.Frame(mainWindow)
-dateFrame.grid(row=4, column=0 sticky='new')
+dateFrame.grid(row=4, column=0, sticky='new')
 # Date labels.
 dayLabel = tkinter.Label(dateFrame, text="Day")
 monthLabel = tkinter.Label(dateFrame, text="Month")
 yearLabel = tkinter.Label(dateFrame, text="Year")
-dayLabel.grid(row=0, column=0, sticky= 'w')
+dayLabel.grid(row=0, column=0, sticky='w')
 monthLabel.grid(row=0, column=1, sticky='w')
 yearLabel.grid(row=0, column=2, sticky='w')
 # Date spinners.
 daySpin = tkinter.Spinbox(dateFrame, width=5, from_=1, to=31)
-monthSpin = tkinter.Spinbox(dateFrame, width=5, values=("Jan, Feb", ))
+monthSpin = tkinter.Spinbox(dateFrame, width=5, values=(
+    "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))
+yearSpin = tkinter.Spinbox(dateFrame, width=5, from_=2000, to=2099)
+daySpin.grid(row=1, column=0)
+monthSpin.grid(row=1, column=1)
+yearSpin.grid(row=1, column=2)
+
+# Buttons
+okButton = tkinter.Button(mainWindow, text="Ok")
+cancelButton = tkinter.Button(
+    mainWindow, text="Cancel", command=mainWindow.destroy)
+okButton.grid(row=4, column=3, sticky='e')
+cancelButton.grid(row=4, column=4, sticky='w')
 
 mainWindow.mainloop()
 

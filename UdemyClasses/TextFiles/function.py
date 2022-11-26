b55efdf853793ec0b98f56ded2a514b8dbc08ca5
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-11-26 17:50:38 -0500

    Function lesson.

---
+++ UdemyClasses/TextFiles/function.py
@@ -0,0 +1,18 @@
+""" Practicing functions in python."""
+def python_food():
+    Width = 80
+    text = "Spam and Eggs"
+    left_margin = (WIDTH - len(text)) // 2
+    print(" " * left_margin, text)
+    
+
+""" Centering spam and eggs."""
+def centre_text(text):
+    left_margin = (width - len(text)) // 2
+    print(" " * left_margin, text)
+    
+    
+# call the function
+centre_text("spam and eggs")
+centre_text("spam, spam, and eggs")
+centre_text("spam, spam, spam, and spam")

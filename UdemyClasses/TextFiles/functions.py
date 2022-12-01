c1284ecae7bf783fe17531c9e22053d906266aeb
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-12-01 04:19:28 -0500

    From line 36 down, calling the variable in the print.

--- UdemyClasses/TextFiles/functions.py
+++ UdemyClasses/TextFiles/functions.py
@@ -21,11 +21,23 @@ def centre_text(*args, sep=' '):
 
 
 # call the function
-print(centre_text("spam and eggs"))
-print(centre_text("spam, spam, and eggs"))
-print(centre_text(12))
-print(centre_text("spam, spam, spam, and spam"))print()
-print(centre_text("first", "second", 3, 4, "spam", sep=":"))
+# s1 = (centre_text("spam and eggs"))
+# print(s1)
+# s2 = (centre_text("spam, spam, and eggs"))
+# print(s2)
+# s3 = (centre_text(12))
+# print(s3)
+# s4 = (centre_text("spam, spam, spam, and spam"))
+# print(s4)
+# s5 = (centre_text("first", "second", 3, 4, "spam", sep=":"))
+# print(s5)
 
-print("=" + str(12 * 3))
-print(sorted(["b", "d", "c", "a"]))
+with open("menu", "w") as menu:
+    s1 = (centre_text("spam and eggs"))
+    print(s1, file=menu)
+    s2 = (centre_text("spam, spam, and eggs"))
+    print(s2, file=menu)
+    print(centre_text(12))
+    print(centre_text("spam, spam, spam, and spam"), file=menu)
+    s5 = (centre_text("first", "second", 3, 4,  "spam", sep=":"))
+    print(s5, file=menu)

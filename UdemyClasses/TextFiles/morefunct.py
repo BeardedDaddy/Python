9d26e9eed4135d16d962d54956a7abe741db893f
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-12-04 17:18:36 -0500

    Made corrections to code then had to reboot vs code to run.

--- UdemyClasses/TextFiles/morefunct.py
+++ UdemyClasses/TextFiles/morefunct.py
@@ -9,35 +9,29 @@ except ImportError:  # python 2
     import Tkinter as tkinter
 
 
-def parablola(n):
-    s = n * n
-    return s
+def parablola(x):
+    y = x * x
+    return y
 
 
-def draw_anes(_canvas):
+def draw_axes(_canvas):
     canvas.update()
-    n_origin = canvas.winfo_width() / 2
-    s_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-n_origin, -s_origin, n_origin, s_origin))
-    _canvas.create_line(-n_origin, 0, -s_origin, fill="white")
-
-
-def plot(_canvas, n, s):
-    canvas.create_line(n, s, n + 1, s + 1, fill="red")
-
+    x_origin = canvas.winfo_width() / 2
+    y_origin = canvas.winfo_height() / 2
+    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="white")
+    canvas.create_line(0, y_origin, 0, -y_origin, fill="white")
 
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Parablola")
-mainWindow.geometry("640n480")
+mainWindow.geometry("640x480")
 
 canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
-draw_anes(canvas)
-
-for n in range(-100, 100):
-    s = parablola(n)
-    plot(canvas, n, s)
+for x in range(-100, 100):
+    y = parablola(x)
+    print(y)
 
 mainWindow.mainloop()

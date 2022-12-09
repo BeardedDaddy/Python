d7185f308e01c05c4f239bc1e43faee8de1e4f22
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-12-09 08:05:24 -0500

    Having issues with the import of Tkinter.

--- UdemyClasses/Blackjack/blackjack.py
+++ UdemyClasses/Blackjack/blackjack.py
@@ -1,8 +1,35 @@
 import random
-import tkinter
-import Tkinter as tkinter
+
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
 
 mainWindow = tkinter.Tk()
+
+
+def load_images(card_images):
+    suits = ['heart', 'club', 'diamond', 'spades']
+    face_cards = ['jack', 'queen', 'king']
+
+    if tkinter.Tkinter >= 8.6:
+        extension = 'png'
+    else:
+        extension = 'ppm'
+
+    # For each suit, retrieve the image for the cards
+    for suit in suits:
+        # First the number cards 1 to 10
+        for card in range(1, 11):
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image,))
+
+        # Next the face cards
+        for card in face_cards:
+            name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
+            card_images.append((10, image,))
+
 
 # Set up the screen and frames for the dealer and player
 mainWindow.title("Black Jack")
@@ -20,7 +47,7 @@ tkinter.Label(card_frame, text="Dealer", background="green",
               fg='white').grid(row=0, column=0)
 tkinter.Label(card_frame, textvariable=dealer_score_label,
               background="green", fg="white").grid(row=1, column=0)
-# Embedded frame hold the card images.
+# Embedded frame to hold the card images.
 dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
 
@@ -29,3 +56,22 @@ tkinter.Label(card_frame, text="Player", background="green",
               fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label,
               background="green", fg="white").grid(row=3, column=0)
+# Embedded frame to hold the card images
+player_card_frame = tkinter.Frame(card_frame, background="green")
+player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
+
+dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
+
+# Load cards
+cards = []
+load_images(cards)
+print(cards)
+
+mainWindow.mainloop()

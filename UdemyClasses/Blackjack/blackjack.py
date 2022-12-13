8703dd392824e57ae261ba1634d8fa191d5b520a
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2022-12-13 05:43:17 -0500

    Searching for TclError fix (can't open cards).

--- UdemyClasses/Blackjack/blackjack.py
+++ UdemyClasses/Blackjack/blackjack.py
@@ -1,5 +1,7 @@
 import random
 
+import cards
+
 try:
     import tkinter
 except ImportError:  # python 2
@@ -29,8 +31,8 @@ def load_images(card_images):
         for card in face_cards:
             name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
-            card_images.append((card, image,))
-            image = tkinter.PhotoImage(file=name)
+            # card_images.append((card, image,))
+            # image = tkinter.PhotoImage(file=name)
             card_images.append((10, image,))
 
 
@@ -64,7 +66,7 @@ card_frame = tkinter.Frame(mainWindow, relief="sunken",
 card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
-tkinter.Label(card_frame, textvariable="Dealer", background="green",
+tkinter.Label(card_frame, text="Dealer", background="green",
               fg='white').grid(row=0, column=0)
 tkinter.Label(card_frame, text=dealer_score_label,
               background="green", fg="white").grid(row=1, column=0)
@@ -85,11 +87,11 @@ button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
 dealer_button = tkinter.Button(
-    button_frame, text="Dealer", command=deal_dealer)
+    button_frame, text="Dealer")
 dealer_button.grid(row=0, column=0)
 
 player_button = tkinter.Button(
-    button_frame, text="Player", command=deal_player)
+    button_frame, text="Player")
 player_button.grid(row=0, column=1)
 
 # Load cards

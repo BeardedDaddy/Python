9549379814e68c9b7c1c06f76f1066161e0bff7f
Grevy <grevyjr@gmail.com>
2022-12-24 22:11:49 -0500

    Configured Visual Studio. Added Rust extension.

--- UdemyClasses/TextFiles/browser.py
+++
@@ -1,9 +0,0 @@
-""" Learning about the webbrowser module.
-"""
-import webbrowser
-
-# webbrowser.open("https://www.python.org/", new=1)
-
-# # help:(webbrowser)  # type: ignore
-# chrome = webbrowser.get(using='chrome')
-# chrome.open_new("https://www.python.org/")

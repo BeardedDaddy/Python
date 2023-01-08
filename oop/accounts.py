321063b34dccf5192d1749ed8086c7a9e2d936f5
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-01-07 21:05:48 -0500

    Created a new classes that manage bank balance.

--- oop/accounts.py
+++ oop/accounts.py
@@ -4,37 +4,42 @@ import pytz
 class Account:
     """ Simple account class with balance """
 
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
+
     """ This class creates a function for the account holders name and balance """
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transactions_list = []
-        print("Account created for " + self.name)
+        self._name = name
+        self.__balance = balance
+        self._transaction_list = [(Account._current_time(), balance)]
+        print("Account created for " + self._name)
+        self.show_balance()
 
     """ This class creates a function for amount after a deposit """
     def deposit(self,amount):
-        if amount > 0:
-            self.balance += amount
+        if amount >0:
+            self.__balance += amount
             self.show_balance()
-            self.transactions_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
+            self._transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
 
     """ This class creates a function for amount after a withdrawal """
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
         else:
-            print("You are over your acount balance of {}. Please enter a lesser amount? ".format(self.balance))
+            print("You are over your acount balance of {}. Please enter a lesser amount? ".format(self.__balance))
         self.show_balance()
 
     """ This class creates a function to show the balance """
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
 
-
-    def show_transaction(self):
-        for date, amount in self.transactions_list:
-            if amount > 0:
+    def show_transactions(self):
+        for date, amount in self._transaction_list:
+            if amount >0:
                 tran_type = "deposited"
             else:
                 tran_type = "withdrawn"
@@ -51,4 +56,14 @@ if __name__ == '__main__':
     # grevy.show_balance()
 
     grevy.withdraw(900000)
-    grevy.show_transaction()
+    grevy.show_transactions()
+
+    mia = Account("Mia", 300000)
+    mia.balance = 200
+    mia.deposit(10000)
+    mia.withdraw(500)
+    mia.show_transactions()
+    print(mia.__dict__)
+    mia._Account__balance = 40
+    mia.show_balance()
+    

94756e9d78b355789c4b496522f9299eecd30e48
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-01-09 04:07:20 -0500

    Album class with docstrings.

---
+++ oop/demo.py
@@ -0,0 +1,16 @@
+a_string = "this a\na string split\t\tand tabbed"
+print(a_string)
+
+raw_string = r"this is\na string split\t\tand tabbed"
+print(raw_string)
+
+b_string = "this is" + chr(10) + "a string split" + chr(9) + chr(9) + "and tabbed"
+print(b_string)
+
+backslash_string = "this is a backlash \followed by some text"
+print(backslash_string)
+
+backslash_string = "this is a backslash \\followed by some text"
+print(backslash_string)
+
+error_string = r"this string ends with a \\"

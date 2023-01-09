76260d8e69bb27ce247388837ba5c93f2f8c79b5
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-01-09 05:15:05 -0500

    Created an artist class for album module.

--- oop/song.py
+++ oop/song.py
@@ -17,24 +17,24 @@ class Album:
     """ Class to represent an Album, using it's track list
     
     Attributes:
-        album_name (str): The name of the album.
+        name (str): The name of the album.
         year (int): The year was album was released>
         artist: (Artist): The artist responsible for the album.
-            If not specified, the artist will default to an artist with the name
-            "Various Artists".
+        If not specified, the artist will default to an artist with the name "Various Artists".
         tracks (List[Song]): A list of the songs on the album.
         
     Methods:
         add_song: Used to add a new song to the album's track list.
         """
 
-    def __init__(self, name, year, artist=None):
+    def __init__(self, name, year, Artist=None):
         self.name = name
         self.year = year
-        if artist is None:
+        self.artist = Artist
+        if Artist is None:
             self.artist = Artist("Various Artists")
         else:
-            self.artist = artist
+            self.artist = Artist
     
         self.tracks = []
 
@@ -51,4 +51,45 @@ class Album:
             self.tracks.append(song)
         else:
             self.tracks.insert(position, song)
-            
+
+class Artist: 
+    """Basic class to store artist details.
+    
+    Attributes:
+        name (str): The name of the artist.
+        albums (List[Album]): A list of the albums by this artist.
+            The list includes only those albums in this collection, it is 
+            not an exhaustive list of the artist's published albums.
+
+    Methods:
+        add_ablum: Use to add a new album to the artist's albums list
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album):
+        """Add a new album to the list.
+        
+        Args:
+            album (Album): Album object to add to the list.
+                If the album is already present, it will not added again (althrough this is yet to implemented).
+        """
+        self.albums.append(album)
+
+
+def load_data():
+    new_artist = None
+    new_album = None
+    artist_list = []
+
+    with open("albums.txt", "r", encoding= "utf-8") as albums:
+        for line in albums:
+            # data row should consist of (artist, album, year, song)
+            artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
+            year_field = int(year_field)
+            print(artist_field, album_field, year_field, song_field)
+
+if __name__=='__main__':
+    load_data()

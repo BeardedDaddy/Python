8c844372fe030e2287d93cf40a9a6605f2568431
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-01-21 07:58:36 -0500

    Learning inheritance.

--- oop/main.py
+++ oop/main.py
@@ -1,13 +1,33 @@
-from enemy import Enemy
+from enemy import Enemy, Troll, Vampyre
 
-random_monster = Enemy("Basic enemy", 12, 1)
-print(random_monster)
+ugly_troll = Troll("Pug")
+print("Ugly troll - {}".format(ugly_troll))
 
-random_monster.take_damage(4)
-print(random_monster)
+another_troll = Troll("Ug")
+print("Another troll - {}".format(another_troll))
+another_troll.take_damage(18)
+print(another_troll)
 
-random_monster.take_damage(8)
-print(random_monster)
+brother = Troll("Urg")
+print(brother)
 
-random_monster.take_damage(9)
-print(random_monster)
+ugly_troll.grunt()
+another_troll.grunt()
+brother.grunt()
+
+vamp = Vampyre("Vlad")
+print(vamp)
+vamp.take_damage(5)
+print(vamp)
+
+print("-" * 40)
+another_troll.take_damage(30)
+print(another_troll)
+
+# while vamp.alive:
+#     vamp.take_damage(1)
+    #print(vamp)
+
+vamp._lives = 0
+vamp._hit_points = 1
+print(vamp)

b60965f5becd1e1b16095a7dfe58c3a40dfcc291
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-01-22 19:04:00 -0500

    Duck Test lesson.

--- oop/main.py
+++ oop/main.py
@@ -1,38 +1,10 @@
-from enemy import Enemy, Troll, Vampyre, VampyreKing
+# This is an integer.
+a = 3
+# This is a string.
+b = "grevy"
+# This is a tuple.
+c = 1, 2, 3
 
-dracula = VampyreKing("Dracula")
-print(dracula)
-dracula.take_damage(12)
-print(dracula)
-
-# ugly_troll = Troll("Pug")
-# print("Ugly troll - {}".format(ugly_troll))
-
-# another_troll = Troll("Ug")
-# print("Another troll - {}".format(another_troll))
-# another_troll.take_damage(18)
-# print(another_troll)
-
-# brother = Troll("Urg")
-# print(brother)
-
-# ugly_troll.grunt()
-# another_troll.grunt()
-# brother.grunt()
-
-# vamp = Vampyre("Vlad")
-# print(vamp)
-# vamp.take_damage(5)
-# print(vamp)
-
-# print("-" * 40)
-# another_troll.take_damage(30)
-# print(another_troll)
-
-# # while vamp.alive:
-# #     vamp.take_damage(1)
-#     #print(vamp)
-
-# vamp._lives = 0
-# vamp._hit_points = 1
-# print(vamp)
+print(a)
+print(b)
+print(c)

30d6b2f77a87feacc2b5b0e8c9a1e3619be1e5e5
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-05-27 12:55:17 -0400

    Learning about sqlite account database.

--- Python Examples.py
+++ Python Examples.py
@@ -1,5 +1,10 @@
-def hello():
-    print("Hello")
+marks = [80, 90, 100, 98, 99]
+# Find the average marks.
+# Based on the average marks find the grade.
+# A = >80/ B = >60 - 80/ C = >50 -  60/ F = < 50
 
-    
-hello()
+def average_grade(marks):
+   sum 
+
+
+average_grade(result)

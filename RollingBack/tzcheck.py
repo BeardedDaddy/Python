161e1a1d153346ab860d99c5aa455ae9146089fd
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-07-16 01:31:27 -0400

    In tzcheck module: troubleshoot line 11.

--- RollingBack/tzcheck.py
+++ RollingBack/tzcheck.py
@@ -5,12 +5,11 @@ import pickle
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
-# for row in db.execute("SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
-#                       " history.account, history.amount FROM history ORDER BY history.time"):
+for row in db.execute ("SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime," " history.account, history.amount FROM history ORDER BY history.time"):
 for row in db.execute("SELECT * FROM history"):
     utc_time = row[0]
-    picked_zone = row[1]
-    zone = pickle.loads(picked_zone.encode())
+    picked_zone = row[3]
+    zone = pickle.loads(picked_zone)
     #zone = pytz.timezone("Australia/Adelaide")
     local_time = pytz.utc.localize(utc_time).astimezone(zone)
     print("{}\t{}\t{}".format(utc_time, local_time, local_time.tzinfo))

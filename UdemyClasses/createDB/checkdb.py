f5436c26c843253c13b49441a294c07ff5e32ead
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-09-24 14:14:55 -0400

    Learning about cursor connections in sqlite3.

--- UdemyClasses/createDB/checkdb.py
+++ UdemyClasses/createDB/checkdb.py
@@ -2,11 +2,10 @@ import sqlite3
 
 conn = sqlite3.connect("contacts.sqlite")
 
-name = input("Please enter a name to search for ")
+#name = input("Please enter a name to search for ")
 
 # for row in conn.execute("SELECT * FROM contacts WHERE name = ?", (name,)):
-for row in conn.execute("SELECT * FROM contacts WHERE name LIKE ?", (name,)):
-    
+for row in conn.execute("SELECT * FROM contacts"):
     print(row)
     
 conn.close()

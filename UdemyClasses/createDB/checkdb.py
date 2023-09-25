7f0bae8c6f904563d357e65ce9f8a2b3a98a82ce
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-09-25 18:21:00 -0400

    Understanding how to loop thru SQL using a tuple.

--- UdemyClasses/createDB/checkdb.py
+++ UdemyClasses/createDB/checkdb.py
@@ -4,6 +4,10 @@ conn = sqlite3.connect("contacts.sqlite")
 
 name = input("Please enter a name to search for ")
 
-for row in conn.execute("SELECT * FROM contacts WHERE name = ?", (name)):
+for row in conn.execute("SELECT * FROM contacts WHERE name LIKE ?", (name,)):
+# the name at the end in parathesis has comma with it to make it a tuple. 
+# If you do not use a comma you will get a ProgrammingError:
+# Incorrect number of bindings supplied. The current statement uses 1, and there are 5 supplied. #noqa
+# Also in that line you can use LIKE so that the query does not have to be case sensative. #noqa
     print(row)  
 conn.close()

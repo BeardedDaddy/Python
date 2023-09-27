12fbc80ecfb18949daab9df776b3d853cb3e7950
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-09-27 06:50:56 -0400

    Fixed line 2 in my return statement.

---
+++ UdemyClasses/Blackjack/get_ints.py
@@ -0,0 +1,9 @@
+# def getint():
+#     while True:
+#         try:
+#             number = int(input("Please enter a number :"))
+#             return number
+#         except ValueError:
+#             print("Invalid number entered, please try again.")
+            
+x = int(input("Enter a number "))
---
+++ UdemyClasses/DivideTwoNumber.py
@@ -0,0 +1,9 @@
+def DivideTwoNumbers(n1, n2):
+    return n1 / n2
+
+
+print("Pick two numbers to divide: ")
+n1 = input('First Number ')
+n2 = input('Second Number ')
+x = DivideTwoNumbers()
+print("Your answer is ")

75afb0da4e361199b10f0f2be28a570750ee3700
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-10-27 05:59:14 -0400

    Fix the issue with line 51: OperationalError.

--- RollingBack.db/tztest.py
+++ RollingBack.db/tztest.py
@@ -4,20 +4,20 @@ import pytz
 import pickle
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
-db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
+db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")  # noqa
 db.execute("CREATE TABLE IF NOT EXISTS history (time TIMESTAMP NOT NULL,"
            " account TEXT NOT NULL, amount INTEGER NOT NULL,"
            " zone INTEGER NOT NULL, PRIMARY KEY (time, account))")
 db.execute("CREATE VIEW IF NOT EXISTS localhistory AS"
-           " SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
-           " history.account, history.amount FROM history ORDER BY history.time")
+           " SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"  # noqa
+           " history.account, history.amount FROM history ORDER BY history.time")  # noqa
 
 
 class Account(object):
 
     @staticmethod
     def _current_time():
-        #return pytz.utc.localize(datetime.datetime.utcnow())
+        # return pytz.utc.localize(datetime.datetime.utcnow())
         # local_time = pytz.utc.localize(datetime.datetime.utcnow())
         # return local_time.astimezone()
 
@@ -27,7 +27,7 @@ class Account(object):
         return utc_time, zone
 
     def __init__(self, name: str, opening_balance: int = 0):
-        cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", 
+        cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)",  # noqa
                             (name,))
         row = cursor.fetchone()
 
@@ -37,18 +37,18 @@ class Account(object):
         else:
             self.name = name
             self._balance = opening_balance
-            cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, opening_balance))
+            cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, opening_balance))  # noqa
             cursor.connection.commit()
             print("Account created for {}. ".format(self.name), end='')
         self.show_balance()
 
     def _save_update(self, amount):
         new_balance = self._balance + amount
-        deposit_time, zone = Account._current_time()  # <-- unpack the returned tuple
+        deposit_time, zone = Account._current_time()  # <-- unpack the returned tuple  # noqa
         picked_zone = pickle.dumps(zone)
-        
-        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))  # noqa: E501
-        db.execute("INSERT INTO history VALUES(?, ?, ?, ?)", (deposit_time, self.name, amount, picked_zone)) # noqa: E501
+
+        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))  # noqa
+        db.execute("INSERT INTO history VALUES(?, ?, ?, ?)", (deposit_time, self.name, amount, picked_zone))  # noqa  # To fix this find out why this is having a problem. Look at the history. 
         db.commit()
         self._balance = new_balance
 
@@ -56,9 +56,9 @@ class Account(object):
         if amount > 0.0:
             # new_balance = self._balance + amount
             # deposit_time = Account._current_time()
-            # db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", 
+            # db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)",  # noqa
             # (new_balance, self.name))
-            # db.execute("INSERT INTO history VALUES(?, ?, ?)", 
+            # db.execute("INSERT INTO history VALUES(?, ?, ?)",
             # (deposit_time, self.name, amount))
             # db.commit()
             # self._balance = new_balance
@@ -70,9 +70,9 @@ class Account(object):
         if 0 < amount <= self._balance:
             # new_balance = self._balance - amount
             # withdrawal_time = Account._current_time()
-            # db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", 
+            # db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)",  # noqa
             # (new_balance, self.name))
-            # db.execute("INSERT INTO history VALUES(?, ?, ?)", 
+            # db.execute("INSERT INTO history VALUES(?, ?, ?)",
             # (withdrawal_time, self.name, -amount))
             # db.commit()
             # self._balance = new_balance
@@ -80,15 +80,16 @@ class Account(object):
             print("{:.2f} withdrawn".format(amount / 100))
             return amount / 100
         else:
-            print("The amount must be greater than zero and no more than your account balance")
+            print("The amount must be greater than zero and no more than your account balance")  # noqa
             return 0.0
 
     def show_balance(self):
-        print("Balance on account {} is {:.2f}".format(self.name, self._balance / 100))
+        print("Balance on account {} is {:.2f}".format(self.name, self._balance / 100))  # noqa
+
 
 if __name__ == '__main__':
     john = Account("John")
-    john.deposit(100.00)
+    john.deposit(100)
     john.deposit(10)
     john.deposit(10)
     john.withdraw(30)

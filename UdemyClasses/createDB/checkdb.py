de671121d7f795a84f2b3df5af2a45717a703344
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2023-11-24 19:45:28 -0500

    Lsn 381 done. ? placeholders to prevent injects.

--- UdemyClasses/createDB/checkdb.py
+++ UdemyClasses/createDB/checkdb.py
@@ -2,12 +2,12 @@ import sqlite3
 
 conn = sqlite3.connect("contacts.sqlite")
 
-name = input("Please enter a name to search for ")
+# name = input("Please enter a name to search for ")
 
-for row in conn.execute("SELECT * FROM contacts WHERE name LIKE ?", (name,)):
+for row in conn.execute("SELECT * FROM sqlite_master"):
 # the name at the end in parathesis has comma with it to make it a tuple. 
 # If you do not use a comma you will get a ProgrammingError:
 # Incorrect number of bindings supplied. The current statement uses 1, and there are 5 supplied. #noqa
 # Also in that line you can use LIKE so that the query does not have to be case sensative. #noqa
-    print(row)  
+    print(row)
 conn.close()

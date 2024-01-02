03696de3b6d2a05702f7c37f323c8ff730484264
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-01-02 06:45:00 -0500

    Learned about *args in lesson 402 of Udemy.

---
+++ UdemyClasses/Lesson_402_Challenge.md
@@ -0,0 +1,3 @@
+# Write a function called build_tuple that takes a variable number of arguments, and returns a tuple containing the values passed to it
+
+The image shows some sample input to the function, and the expected output:
--- UdemyClasses/star_args.py
+++ UdemyClasses/star_args.py
@@ -1,13 +1,27 @@
-# """This function creates a class that builds a tuple with the argument * args."""  # noqa
-# def build_tuple(*args):
-    # return args
-# 
-# message_tuple = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")  # noqa: E501
-# print(type(message_tuple))
-# print(message_tuple)
-# 
-# number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
-# print(type(number_tuple))
-# print(number_tuple)
+ """This function creates a class that builds a tuple with the argument * args."""  # noqa
+ def build_tuple(*args):
+   # return args
 
-print("Hello", "planet", "earth")
+ message_tuple = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")  # noqa: E501
+ print(type(message_tuple))
+ print(message_tuple)
+
+ number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
+ print(type(number_tuple))
+ print(number_tuple)
+
+#def average(args):
+#    """This function is class called average with an argument of * args."""
+#    # The * unpacks the args argument as tuple (average(1, 2, 3, 4))
+#    # Without the * you get a TypeError indicating that 1 positional argument but 4 were given.  # noqa
+#    # To print it without the *args you would have to put a second set of parenthesis around the ((1, 2, 3, 4))  # noqa
+#    print(type(args))
+#    print(f"args is {args}:")
+#    print("*args is:", *args)
+#    mean = 0
+#    for arg in args:
+#        mean += arg
+#    return mean / len(args)
+#
+#
+#print(average((1, 2, 3, 4)))

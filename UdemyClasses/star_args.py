eaa523aaa511c14e8058d3a7f146959b78413c2e
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-01-03 05:16:23 -0500

    Add backwards.txt and update PythonImportHelper and star_args

--- UdemyClasses/star_args.py
+++ UdemyClasses/star_args.py
@@ -1,20 +1,22 @@
- """This function creates a class that builds a tuple with the argument * args."""  # noqa
- def build_tuple(*args):
-   # return args
+# """This function creates a class that builds
+# a tuple with the argument * args."""  # noqa
+# def build_tuple(*args):
+#    return args
+# message_tuple = build_tuple("hello", "planet", "earth",
+# "take", "me", "to", "your", "leader")  # noqa: E501
+# print(type(message_tuple))
+# print(message_tuple)
+# number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
+# print(type(number_tuple))
+# print(number_tuple)
 
- message_tuple = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")  # noqa: E501
- print(type(message_tuple))
- print(message_tuple)
-
- number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
- print(type(number_tuple))
- print(number_tuple)
-
-#def average(args):
+# def average(args):
 #    """This function is class called average with an argument of * args."""
 #    # The * unpacks the args argument as tuple (average(1, 2, 3, 4))
-#    # Without the * you get a TypeError indicating that 1 positional argument but 4 were given.  # noqa
-#    # To print it without the *args you would have to put a second set of parenthesis around the ((1, 2, 3, 4))  # noqa
+#    # Without the * you get a TypeError indicating that 1
+# positional argument but 4 were given.  # noqa
+#    # To print it without the *args you would have to put a
+# second set of parenthesis around the ((1, 2, 3, 4))  # noqa
 #    print(type(args))
 #    print(f"args is {args}:")
 #    print("*args is:", *args)
@@ -24,4 +26,45 @@
 #    return mean / len(args)
 #
 #
-#print(average((1, 2, 3, 4)))
+# print(average((1, 2, 3, 4)))
+
+
+#def print_backwards(*args, end=' ', **kwargs):
+#def print_backwards(*args, **kwargs):
+#    """This function creates a class called print_backwards 
+#    with *args and **kwargs as the argument."""  # noqa+
+#    print(kwargs)
+#    kwargs.pop('end', None)
+#    for word in args[::-1]:
+#        print(word[::-1], end=' ', **kwargs)
+
+
+def print_backwards(*args, **kwargs):
+    """This function creates a class called print_backwards 
+    with *args and **kwargs as the argument."""  # noqa+
+    end_character = kwargs.pop('end', '\n')
+    sep_character = kwargs.pop('sep', ' ')
+    for word in args[:0:-1]:    # change the range
+        print(word[::-1], end=sep_character, **kwargs)
+    print(args[0][::-1], end=end_character, **kwargs)   # and print the first word seperately.  # noqa
+#    print(end=end_character)   # which means we don't need this line
+
+
+def backwards_print(*args, **kwargs):
+    """This function creates a class called backwards_print 
+    with *args and **kwargs as the argument."""  # noqa+
+    sep_character = kwargs.pop('sep', ' ')
+    print(sep_character.join(word[::-1] for word in args[::-1]), **kwargs)
+
+with open("backwards.txt", 'w', encoding='utf-8') as backwards:
+    print_backwards(
+        "Hello planet earth take me to your leader", end='\n')
+    print("Another String")
+
+
+print()
+print_backwards(
+    "Hello", "planet", "earth", "take", "me", "to", "your", "leader", end=' ', sep='\n**\n')
+print(
+    "Hello", "planet", "earth", "take", "me", "to", "your", "leader", end=' ', sep='\n**\n')
+print("=" * 10)

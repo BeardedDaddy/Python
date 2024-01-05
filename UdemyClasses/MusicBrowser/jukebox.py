26466fe64cef7f3119ff8ebf8f44f33c838bf004
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-01-05 06:19:56 -0500

    I got the modules from Udemy source code and deleted the ones I created.

--- UdemyClasses/MusicBrowser/jukebox.py
+++ UdemyClasses/MusicBrowser/jukebox.py
@@ -1,4 +1,6 @@
-"""The line below imports the sqlite3 module."""
+""" This module uses the sqlite3 module to interact with a SQLite database.
+"""
+
 import sqlite3
 import tkinter
 
@@ -6,20 +8,43 @@ conn = sqlite3.connect('music.sqlite')
 
 
 class Scrollbox(tkinter.Listbox):
-    """This class creates a function called Scrollbox with the method called tkinter.Listbox."""  # noqa
+    """ Line 11 creates a class called Scrollbox that has method as argument.
+    """
+
     def __init__(self, window, **kwargs):
-        # tkinter.Listbox.__init__(self, window, **kwargs) # Python 2
+        # tkinter.Listbox.__init__(self, window, **kwargs)  # Python 2
         super().__init__(window, **kwargs)
 
-        self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, command=self.yview)  # noqa
+        self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL,
+                                           command=self.yview)
 
-
-    def grid(self, row, column, sticky='nsw', rowspan=1, columnspan=1, **kwargs):  # noqa
-        """This line creates a function called grid that has several arguments."""  # noqa
-        # tkinter.Listbox.gridself, row=row, column=column, sticky=sticky, rowspan=rowspan, **kwargs) # Python 2  # noqa
-        super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, columnspan=columnspan, **kwargs)  # noqa
-        self.scrollbar.grid(row=row, column=column, sticky='nse', rowspan=rowspan)  # noqa
+    def grid(self, row, column, sticky='nsw',
+             rowspan=1, columnspan=1, **kwargs):
+        # tkinter.Listbox.grid(self, row=row, column=column, sticky=sticky,
+        # rowspan=rowspan,
+        #  **kwargs)  # Python 2
+        super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan,
+                     columnspan=columnspan, **kwargs)
+        self.scrollbar.grid(row=row, column=column, sticky='nse',
+                            rowspan=rowspan)
         self['yscrollcommand'] = self.scrollbar.set
+
+
+def get_albums(event):
+    """ This line of code creates a function called def get_albums()
+with a argument called event.
+"""
+
+    lb = event.widget
+    index = lb.curselection()[0]
+    artist_name = lb.get(index),
+
+    # get the artist ID from the database row
+    artist_id = conn.execute
+    ("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()alist = []
+    for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", artist_id):
+        alist.append(row[0])
+    albumLV.set(tuple(alist))
 
 
 mainWindow = tkinter.Tk()
@@ -29,7 +54,7 @@ mainWindow.geometry('1024x768')
 mainWindow.columnconfigure(0, weight=2)
 mainWindow.columnconfigure(1, weight=2)
 mainWindow.columnconfigure(2, weight=2)
-mainWindow.columnconfigure(3, weight=2)  # spacer column on right
+mainWindow.columnconfigure(3, weight=1)    # spacer column on right
 
 mainWindow.rowconfigure(0, weight=1)
 mainWindow.rowconfigure(1, weight=5)
@@ -37,14 +62,19 @@ mainWindow.rowconfigure(2, weight=5)
 mainWindow.rowconfigure(3, weight=1)
 
 # ===== labels =====
-tkinter.Label(mainWindow, text="Artist").grid(row=0, column=0)
+tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
 tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
 # ===== Artists Listbox =====
-artistList = Scrollbox(mainWindow, background="green")
+artistList = Scrollbox(mainWindow)
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
+
+for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
+    artistList.insert(tkinter.END, artist[0])
+
+artistList.bind('<<ListboxSelect>>', get_albums)
 
 # ===== Albums Listbox =====
 albumLV = tkinter.Variable(mainWindow)
@@ -60,7 +90,7 @@ songList = Scrollbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 
-# ===== Main Loop =====
+# ===== Main loop =====
 testList = range(0, 100)
 albumLV.set(tuple(testList))
 mainWindow.mainloop()

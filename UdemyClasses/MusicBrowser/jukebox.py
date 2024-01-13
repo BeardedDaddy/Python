170704790257a30eb66d9c66b51a8a963db21a5b
Grevy <grevyjr@gmail.com>
2024-01-13 09:32:53 -0500

    Fixed the issues with the datalistbox class on line 37.

--- UdemyClasses/MusicBrowser/jukebox.py
+++ UdemyClasses/MusicBrowser/jukebox.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect('music.sqlite')
 
 
 class Scrollbox(tkinter.Listbox):
-    """ Line 11 creates a class called Scrollbox that has method as argument.
+    """ Line 10 creates a class called Scrollbox that has method as argument.
     """
 
     def __init__(self, window, **kwargs):
@@ -18,11 +18,12 @@ class Scrollbox(tkinter.Listbox):
         self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL,
                                            command=self.yview)
 
+    def grid(self, row, column, sticky='nsw',
+             rowspan=1, columnspan=1, **kwargs):
         """ This function creates a grid with the parameters
         of row, column, and sticky.
         """
-    def grid(self, row, column, sticky='nsw',
-             rowspan=1, columnspan=1, **kwargs):
+
         # tkinter.Listbox.grid(self, row=row, column=column, sticky=sticky,
         # rowspan=rowspan,
         #  **kwargs)  # Python 2
@@ -33,13 +34,55 @@ class Scrollbox(tkinter.Listbox):
         self['yscrollcommand'] = self.scrollbar.set
 
 
+class DataListBox(Scrollbox):
+    """ Line 36 creates a class called DataListBox that has method as argument.
+    """
+    def __init__(self, window, connection, table, field, sort_order=(), **kwargs):  # noqa
+        # Scrollbox.__init__(self, window, **kwargs)  # Python 2
+        super().__init__(window, **kwargs)
+
+        self.cursor = connection.cursor()
+        self.table = table
+        self.field = field
+
+        self.sql_select = "SELECT " + self.field + ", _id" + " FROM " + self.table  # noqa
+        if sort_order:
+            self.sql_sort = " ORDER BY " + ",".join(sort_order)
+        else:
+            self.sql_sort = " ORDER BY " + self.field
+
+    def clear(self):
+        """clear class, is used to clear the listbox.
+        """
+        self.delete(0, tkinter.END)
+
+    def requery(self, link_value=None):
+        """requery class, is used to requery the listbox.
+        """
+        if link_value:
+            sql = self.sql_select + " WHERE " + "artist" + "=?" + self.sql_sort
+            print(sql)   # TODO delete this line
+            self.cursor.execute(sql, (link_value,))
+        else:
+            print(self.sql_select + self.sql_sort)  # TODO delete this line
+            self.cursor.execute(self.sql_select + self.sql_sort)
+
+        # clear the listbox contents before re-loading
+        self.clear()
+        for value in self.cursor:
+            self.insert(tkinter.END, value[0])
+
+
 def get_albums(event):
     """ This line of code creates a function called def get_albums()
-with a argument called event.
-"""
+    with a argument called event.
+    """
 
     lb = event.widget
     index = lb.curselection()[0]
+    print(lb.get(index))
+
+    # get the artist name from the listbox
     artist_name = lb.get(index),
 
     # get the artist ID from the database row
@@ -48,7 +91,7 @@ with a argument called event.
     for row in conn.execute('SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name', artist_id):  # noqa
         alist.append(row[0])
     albumLV.set(tuple(alist))
-    songLV.set(('Choose and album',))
+    songLV.set(("Choose an album",))
 
 
 def get_songs(event):
@@ -60,7 +103,7 @@ def get_songs(event):
         This function called get_songs creates a parameter called event.
     """
     lb = event.widget
-    index = int(lb.curselection()[0])
+    index = lb.curselection()[0]
     album_name = lb.get(index),
 
     # get the artist ID from the database row
@@ -91,19 +134,18 @@ tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
 # ===== Artists Listbox =====
-artistList = Scrollbox(mainWindow)
+artistList = DataListBox(mainWindow, conn, "artists", "name")
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
 
-for artist in conn.execute('SELECT artists.name FROM artists ORDER BY artists.name'):  # noqa
-    artistList.insert(tkinter.END, artist[0])
-
+artistList.requery()
 artistList.bind('<<ListboxSelect>>', get_albums)
 
 # ===== Albums Listbox =====
 albumLV = tkinter.Variable(mainWindow)
 albumLV.set(("Choose an artist",))
-albumList = Scrollbox(mainWindow, listvariable=albumLV)
+albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))  # noqa
+albumList.requery(12)
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
 
@@ -112,7 +154,8 @@ albumList.bind('<<ListboxSelect>>', get_songs)
 # ===== Songs Listbox =====
 songLV = tkinter.Variable(mainWindow)
 songLV.set(("Choose an album",))
-songList = Scrollbox(mainWindow, listvariable=songLV)
+songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
+songList.requery()
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 

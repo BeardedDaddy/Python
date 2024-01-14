ed1e08bb4604f089d1fad7de75e68b47cfe4467f
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-01-14 17:22:53 -0500

    I completed the jukebox module. The only thing left to do is clear out the warnings and infos.

--- UdemyClasses/MusicBrowser/jukebox.py
+++ UdemyClasses/MusicBrowser/jukebox.py
@@ -4,9 +4,8 @@
 import sqlite3
 import tkinter
 
-conn = sqlite3.connect('music.sqlite')
 
-
+# ===== classes =====
 class Scrollbox(tkinter.Listbox):
     """ Line 10 creates a class called Scrollbox that has method as argument.
     """
@@ -41,9 +40,14 @@ class DataListBox(Scrollbox):
         # Scrollbox.__init__(self, window, **kwargs)  # Python 2
         super().__init__(window, **kwargs)
 
+        self.linked_box = None
+        self.link_field = None
+
         self.cursor = connection.cursor()
         self.table = table
         self.field = field
+
+        self.bind('<<ListboxSelect>>', self.on_select)
 
         self.sql_select = "SELECT " + self.field + ", _id" + " FROM " + self.table  # noqa
         if sort_order:
@@ -56,11 +60,17 @@ class DataListBox(Scrollbox):
         """
         self.delete(0, tkinter.END)
 
+    def link(self, widget, link_field):
+        """link class, is used to link the listbox.
+        """
+        self.linked_box = widget
+        widget.link_field = link_field
+
     def requery(self, link_value=None):
         """requery class, is used to requery the listbox.
         """
-        if link_value:
-            sql = self.sql_select + " WHERE " + "artist" + "=?" + self.sql_sort
+        if link_value and self.link_field:
+            sql = self.sql_select + " WHERE " + "" + self.link_field + "=?" + self.sql_sort  # noqa
             print(sql)   # TODO delete this line
             self.cursor.execute(sql, (link_value,))
         else:
@@ -71,51 +81,29 @@ class DataListBox(Scrollbox):
         self.clear()
         for value in self.cursor:
             self.insert(tkinter.END, value[0])
-
+        if self.linked_box:
+            self.linked_box.clear()
+            
     def on_select(self, event):
-        print(self is event.widget)  # TODO delete this line.
-        """ This line of code creates a function called def get_albums()
-        with a argument called event.
+        """on_select class, is used to select the listbox.
         """
+        if self.linked_box:
+            print(self is event.widget)    # TODO delete this line.
+            index = self.curselection()[0]
 
-        index = self.curselection()[0]
+            # get the artist name from the listbox
+            value = self.get(index),
 
-        # get the artist name from the listbox
-        value = self.get(index),
-
-        # get the artist ID from the database row
-        link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]  # noqa
-        albumList.requery(link_id)
-
-        # artist_id = conn.execute('SELECT artists._id FROM artists WHERE artists.name=?', artist_name).fetchone()  # noqa
-        # alist = []
-        # for row in conn.execute('SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name', artist_id):  # noqa
-        #     alist.append(row[0])
-        # albumLV.set(tuple(alist))
-        # songLV.set(("Choose an album",))
+            # get the artist ID from the database row
+            link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]  # noqa
+            self.linked_box.requery(link_id)
 
 
-def get_songs(event):
-    """get_songs is the function name.
-
-    Parameters
-    ----------
-    event
-        This function called get_songs creates a parameter called event.
-    """
-    lb = event.widget
-    index = int(lb.curselection()[0])
-    album_name = lb.get(index),
-
-    # get the artist ID from the database row
-    album_id = conn.execute('SELECT _albums._id FROM albums WHERE albums.name=?', album_name).fetchone()  # noqa
-    alist = []
-    for x in conn.execute('SELECT songs.title FROM songs WHERE songs.album=? ORDER BY songs.track', album_id):  # noqa
-        alist.append(x[0])
-    songLV.set(tuple(alist))
-
-if __name__=='__main__':
+# ===== main =====
+if __name__ == '__main__':
     conn = sqlite3.connect('music.sqlite')
+
+    # ===== main window =====
     mainWindow = tkinter.Tk()
     mainWindow.title('Music DB Browser')
     mainWindow.geometry('1024x768')
@@ -130,7 +118,8 @@ if __name__=='__main__':
     mainWindow.rowconfigure(2, weight=5)
     mainWindow.rowconfigure(3, weight=1)
 
-    # ===== labels =====
+
+# ===== labels =====
     tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
     tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
     tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
@@ -142,28 +131,32 @@ if __name__=='__main__':
 
     artistList.requery()
 
+    # ===== Scrollbar =====
+    scrollbar = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)  # noqa
+    scrollbar.grid(row=1, column=0, sticky='nse', rowspan=2)
 
-    # ===== Albums Listbox =====
+# ===== Albums Listbox =====
     albumLV = tkinter.Variable(mainWindow)
     albumLV.set(("Choose an artist",))
     albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))  # noqa
-    albumList.requery(12)
     albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
     albumList.config(border=2, relief='sunken')
 
-    albumList.bind('<<ListboxSelect>>', get_songs)
+    # albumList.bind('<<ListboxSelect>>', get_songs)
+    artistList.link(albumList, "artist")
+
+    # albumList.requery()
 
     # ===== Songs Listbox =====
     songLV = tkinter.Variable(mainWindow)
     songLV.set(("Choose an album",))
-    songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
-    songList.requery()
+    songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))  # noqa
     songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
     songList.config(border=2, relief='sunken')
 
+    albumList.link(songList, "album")
+
     # ===== Main loop =====
-    testList = range(0, 100)
-    albumLV.set(tuple(testList))
     mainWindow.mainloop()
     print("closing database connection")
     conn.close()

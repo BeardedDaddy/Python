417fac80847762371418e070226c3bfb88a34703
Grevy <grevyjr@gmail.com>
2024-01-14 09:15:22 -0500

    I fixed line 107. I added int to the variable to fix the OperationalError.

--- UdemyClasses/MusicBrowser/jukebox.py
+++ UdemyClasses/MusicBrowser/jukebox.py
@@ -72,26 +72,27 @@ class DataListBox(Scrollbox):
         for value in self.cursor:
             self.insert(tkinter.END, value[0])
 
+    def on_select(self, event):
+        print(self is event.widget)  # TODO delete this line.
+        """ This line of code creates a function called def get_albums()
+        with a argument called event.
+        """
 
-def get_albums(event):
-    """ This line of code creates a function called def get_albums()
-    with a argument called event.
-    """
+        index = self.curselection()[0]
 
-    lb = event.widget
-    index = lb.curselection()[0]
-    print(lb.get(index))
+        # get the artist name from the listbox
+        value = self.get(index),
 
-    # get the artist name from the listbox
-    artist_name = lb.get(index),
+        # get the artist ID from the database row
+        link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]  # noqa
+        albumList.requery(link_id)
 
-    # get the artist ID from the database row
-    artist_id = conn.execute('SELECT artists._id FROM artists WHERE artists.name=?', artist_name).fetchone()  # noqa
-    alist = []
-    for row in conn.execute('SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name', artist_id):  # noqa
-        alist.append(row[0])
-    albumLV.set(tuple(alist))
-    songLV.set(("Choose an album",))
+        # artist_id = conn.execute('SELECT artists._id FROM artists WHERE artists.name=?', artist_name).fetchone()  # noqa
+        # alist = []
+        # for row in conn.execute('SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name', artist_id):  # noqa
+        #     alist.append(row[0])
+        # albumLV.set(tuple(alist))
+        # songLV.set(("Choose an album",))
 
 
 def get_songs(event):
@@ -103,7 +104,7 @@ def get_songs(event):
         This function called get_songs creates a parameter called event.
     """
     lb = event.widget
-    index = lb.curselection()[0]
+    index = int(lb.curselection()[0])
     album_name = lb.get(index),
 
     # get the artist ID from the database row
@@ -113,55 +114,56 @@ def get_songs(event):
         alist.append(x[0])
     songLV.set(tuple(alist))
 
+if __name__=='__main__':
+    conn = sqlite3.connect('music.sqlite')
+    mainWindow = tkinter.Tk()
+    mainWindow.title('Music DB Browser')
+    mainWindow.geometry('1024x768')
 
-mainWindow = tkinter.Tk()
-mainWindow.title('Music DB Browser')
-mainWindow.geometry('1024x768')
+    mainWindow.columnconfigure(0, weight=2)
+    mainWindow.columnconfigure(1, weight=2)
+    mainWindow.columnconfigure(2, weight=2)
+    mainWindow.columnconfigure(3, weight=1)    # spacer column on right
 
-mainWindow.columnconfigure(0, weight=2)
-mainWindow.columnconfigure(1, weight=2)
-mainWindow.columnconfigure(2, weight=2)
-mainWindow.columnconfigure(3, weight=1)    # spacer column on right
+    mainWindow.rowconfigure(0, weight=1)
+    mainWindow.rowconfigure(1, weight=5)
+    mainWindow.rowconfigure(2, weight=5)
+    mainWindow.rowconfigure(3, weight=1)
 
-mainWindow.rowconfigure(0, weight=1)
-mainWindow.rowconfigure(1, weight=5)
-mainWindow.rowconfigure(2, weight=5)
-mainWindow.rowconfigure(3, weight=1)
+    # ===== labels =====
+    tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
+    tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
+    tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
-# ===== labels =====
-tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
-tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
-tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
+    # ===== Artists Listbox =====
+    artistList = DataListBox(mainWindow, conn, "artists", "name")
+    artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
+    artistList.config(border=2, relief='sunken')
 
-# ===== Artists Listbox =====
-artistList = DataListBox(mainWindow, conn, "artists", "name")
-artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
-artistList.config(border=2, relief='sunken')
+    artistList.requery()
 
-artistList.requery()
-artistList.bind('<<ListboxSelect>>', get_albums)
 
-# ===== Albums Listbox =====
-albumLV = tkinter.Variable(mainWindow)
-albumLV.set(("Choose an artist",))
-albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))  # noqa
-albumList.requery(12)
-albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
-albumList.config(border=2, relief='sunken')
+    # ===== Albums Listbox =====
+    albumLV = tkinter.Variable(mainWindow)
+    albumLV.set(("Choose an artist",))
+    albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))  # noqa
+    albumList.requery(12)
+    albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
+    albumList.config(border=2, relief='sunken')
 
-albumList.bind('<<ListboxSelect>>', get_songs)
+    albumList.bind('<<ListboxSelect>>', get_songs)
 
-# ===== Songs Listbox =====
-songLV = tkinter.Variable(mainWindow)
-songLV.set(("Choose an album",))
-songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
-songList.requery()
-songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
-songList.config(border=2, relief='sunken')
+    # ===== Songs Listbox =====
+    songLV = tkinter.Variable(mainWindow)
+    songLV.set(("Choose an album",))
+    songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
+    songList.requery()
+    songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
+    songList.config(border=2, relief='sunken')
 
-# ===== Main loop =====
-testList = range(0, 100)
-albumLV.set(tuple(testList))
-mainWindow.mainloop()
-print("closing database connection")
-conn.close()
+    # ===== Main loop =====
+    testList = range(0, 100)
+    albumLV.set(tuple(testList))
+    mainWindow.mainloop()
+    print("closing database connection")
+    conn.close()

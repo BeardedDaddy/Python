2aa1a0ede8b4061205cddcdfb112f005867fddfd
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-01-15 14:20:10 -0500

    Udemy lesson 414, learn about next and range.

--- UdemyClasses/size.py
+++ UdemyClasses/size.py
@@ -5,8 +5,10 @@ import sys
 
 def my_range(n: int):
     """
-    >>> my_range(10)
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    This line defines a function named my_range that takes one argument n.
+    The : int part is a type hint that indicates n should be an integer.
+    Type hints are optional in Python and do not enforce
+    the type of the variable, but they can make your code easier to understand.
     """
     print("my_range starts")
     start = 0
@@ -16,18 +18,18 @@ def my_range(n: int):
         start += 1
 
 
-_ = input("line 19")
 big_range = my_range(5)
-_ = input("line 21")
+_ = input("Line 20, hit enter to continue.")
 
+print(next(big_range))
 print(f"big_range is {sys.getsizeof(big_range)} bytes")
 
 # Create a list containing all the values in big_range
 big_list = []
-_ = input("line 27")
+_ = input("Line 27, hit enter to continue.")
 
 for val in big_range:
-    _ = input("line 30 - text inside loop")
+    _ = input("Line 30, hit enter to continue.")
     big_list.append(val)
 
 print(f"big_list is {sys.getsizeof(big_list)} bytes")

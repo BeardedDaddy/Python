0ccb24ac5e51edce9ded1289c65f90c3dcc7bff0
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-02-07 06:59:05 -0500

    Learning about timeit.

--- UdemyClasses/Comprehension/timeitchallenge.py
+++ UdemyClasses/Comprehension/timeitchallenge.py
@@ -9,9 +9,11 @@
 # of one million will take a long time to run.
 
 import timeit
+from statistics import mean, stdev
 
 
 def fact(n):
+    """ This function returns the factorial."""
     result = 1
     if n > 1:
         for f in range(2, n + 1):
@@ -20,8 +22,18 @@ def fact(n):
 
 
 def factorial(n):
+    """ This function returns the factorial"""
     # n! can also be defined as n * (n-1)!
     if n <= 1:
         return 1
     else:
         return n * factorial(n-1)
+
+
+if __name__ == "__main__":
+    list1 = timeit.repeat("x = fact(130)", setup="from __main__ import fact", number = 10000, repeat = 6)  # noqa
+    list2 = timeit.repeat("x = factorial(130)", setup="from __main__ import factorial", number = 10000, repeat = 6)  # noqa
+    print(mean(list1), stdev(list1))
+    print(mean(list2), stdev(list2))
+# The above code gives the timeit module access to functions you define,
+# you can pass a setup parameter which contains an import statement.

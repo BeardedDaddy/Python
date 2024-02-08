ce4792d51e1ab0be44a806f25fad592a2513b601
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-02-08 06:23:16 -0500

    On line 50 we ran the timeit function for all the functions. Starting on line 57 we ran the timeit function for each function.

--- UdemyClasses/map/map_intro.py
+++ UdemyClasses/map/map_intro.py
@@ -1,3 +1,8 @@
+"""
+The code below imports the timeit module.
+"""
+import timeit
+
 """The code below is a variable that has a string"""
 TEXT = "what have the romans ever done for us"
 
@@ -47,3 +52,13 @@ if __name__ == '__main__':
     print(map_caps())
     print(comp_words())
     print(map_words())
+    # print(timeit.timeit("comp_caps()", setup="from map_intro import comp_caps", number=10000))  # noqa
+    # The above line of code runs the timeit function for all the functions in this module.  # noqa
+    print(timeit.timeit(comp_caps, number=10000))
+    print(timeit.timeit(map_caps, number=10000))
+    print(timeit.timeit(comp_words, number=10000))
+    print(timeit.timeit(map_words, number=10000))
+    # The above lines of code runs the timeit function for each function seperately.  # noqa
+    # In the print statements when passing the function we are not using
+    # parentheses. We're passing a reference to the function, not a function
+    # call. If you were to try to call the function, you'll get an error.

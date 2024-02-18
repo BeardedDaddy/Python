35b0cf71c72464b1045dd8793ca80fb04bcd5b19
Grevy <grevyjr@gmail.com>
2024-02-17 20:06:23 -0500

    Learning about lambda conditionl expressions.

--- UdemyClasses/planting _instructions.py
+++ UdemyClasses/planting _instructions.py
@@ -1,7 +1,10 @@
 """
-The code above will imort from the module data.py the PLANTS_LIST.
+The code above will import from the module data.py the PLANTS_LIST.
 """
-from DATA import PLANTS_LIST
+try:
+    from data import plants_list
+except ImportError:
+    plants_list = []  # Define PLANTS_LIST if it doesn't exist in the data module  # noqa
 
 # with open("PLANTING_INSTRUCTIONS.txt", 'w', encoding='utf-8') as output_file:
 #     for plant in PLANTS_LIST:
@@ -11,21 +14,27 @@ from DATA import PLANTS_LIST
 #         else:
 #             WHERE_TO_PLANT = 'garden'
 
+
 def sort_perinnials(item) -> str:
     if item.lifecycle.casefold() == 'perinnial':
         return '1' + item.name
     else:
         return '0' + item.name
-          
-with open("PLANTING_INSTRUCTIONS.txt", 'w', encoding='utf-8') as output_file:
-    for plant in PLANTS_LIST:
+
+
+# plants_list.sort(key=sort_perinnials)
+plants_list.sort(key=lambda item: '1' + item.name if item.lifecycle ==
+                 'Perinnial' else '0' + item.name)
+
+with open("planting_instructions.txt", 'w', encoding='utf-8') as output_file:
+    for plant in plants_list:
         WHERE_TO_PLANT, who = ('window box', 'me') if plant.lifecycle == 'Perinnial' else ('garden', 'gardner')  # noqa
         # if plant.lifecycle == 'Perennial':
         #     WHERE_TO_PLANT = 'window box'
         #     who = 'me'
-            
+
         # else:
         #     WHERE_TO_PLANT = 'garden'
         #     who = 'gardener'
-            
+
         print(f"{plant.name}: {WHERE_TO_PLANT}, {who}", file=output_file)

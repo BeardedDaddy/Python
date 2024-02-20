80bdcad0faa8284adf24dd78026ed7c1962af575
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-02-20 06:48:15 -0500

    I fixed the problem on line 12, I indented the on_click function on lines 15 - 17.

--- UdemyClasses/calculator_widgets.py
+++ UdemyClasses/calculator_widgets.py
@@ -11,25 +11,25 @@ class CalculatorButton(tk.Button):
         super().__init__(master, **kwargs)
         self.config(command=self.on_click)
 
-        
-def on_click(self):
-    if self.callback:
-        self.callback(self['text'])
- 
- 
+
+    def on_click(self):
+        if self.callback:
+            self.callback(self['text'])
+
+
 def test():
     def clicked(caption: str):
         print(f'{caption} was clicked')
-        
+
     main_window = tk.Tk()
     main_window.title("CalculatorButton test")
     main_window.geometry('640x480')
-    
+
     btn = CalculatorButton(main_window, callback=clicked, text='Test')
     btn.grid(row=0, column=0, sticky="nsew")
-    
+
     main_window.mainloop()
-    
-    
+
+
 if __name__ == '__main__':
     test()

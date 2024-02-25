272979fc453906896b769129619b73aa195cf75e
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-02-25 12:38:13 -0500

    Added the animated bubble sort module.

--- Big_O/bubble_sort.py
+++ Big_O/bubble_sort.py
@@ -2,17 +2,26 @@ def bubble_sort(data: list) -> None:
     """Sorts a list in place."""
     n = len(data)
     comparison_count = 0
-    
+
     for i in range(n -1):
-        for i in range(n - 1):
+        for j in range(n - 1):
             comparison_count += 1
             if data[j] > data[j + 1]:
                 data[j], data[j + 1] = data[j + 1], data[j]
-                
-                
-x = 1
-y = 2
-print(x, y)
+         
+        print(f"End of pass {i}. 'data' is now {data}")
+    print(f"comparison_count is {comparison_count}")
+    
+    
+numbers = [3, 2, 4, 1, 5, 7, 6]
 
-x, y = y, x
-print(x, y)
+print(f"Sorting {numbers}")
+bubble_sort(numbers)
+print(f"The sorted data is {numbers}")
+
+# x = 1
+# y = 2
+# print(x, y)
+
+# x, y = y, x
+# print(x, y)

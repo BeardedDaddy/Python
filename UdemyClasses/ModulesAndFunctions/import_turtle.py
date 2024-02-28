90b50c842ec38c233a2fcbdac460c49fbd2fa499
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-02-28 06:26:53 -0500

    Understanding __name__ == “__main__” in Python.

--- UdemyClasses/ModulesAndFunctions/import_turtle.py
+++ UdemyClasses/ModulesAndFunctions/import_turtle.py
@@ -7,15 +7,15 @@
 # turtle.right(90)
 # turtle.done()
 
-import turtle
-import math
+from turtle import *
+from math import radians, cos
 
 
 def square(length: int) -> None:
     """Draws a square of length `length`"""
     for side in range(4):
-        turtle.forward(length)
-        turtle.right(90)
+        forward(length)
+        right(90)
 
 
 def encircled_square(length: int) -> None:
@@ -23,15 +23,24 @@ def encircled_square(length: int) -> None:
     then encloses it in a circle.
     """
     square(length)
-    angle = math.radians(45)
-    radius = length * math.cos(angle)
-    turtle.right(135)
-    turtle.circle(radius)
+    angle = radians(45)
+    radius = length * cos(angle)
+    right(135)
+    circle(radius)
+    left(135)
+    print(f'Inside function, namespace is: {dir}')
+    print(f'locals: {locals()}')
 
+# encircled_square(300)
+speed('fast')
+for s in range(72):
+    encircled_square(100)
+    left(5)
 
-encircled_square(300)
-# for s in range(72):
-#     square(120)
-#     turtle.left(5)
+done()
 
-turtle.done()
+print(dir())
+g = globals()
+print(g['square'])
+
+print(dir(__builtins__))

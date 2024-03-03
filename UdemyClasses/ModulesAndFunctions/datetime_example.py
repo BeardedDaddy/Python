ff93d1acb65c84ce1e88e0c16fb92f0d87a1599d
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-03 16:35:02 -0500

    Learning how to use the tzone import. I had to pip install tzone package.

--- UdemyClasses/ModulesAndFunctions/datetime_example.py
+++ UdemyClasses/ModulesAndFunctions/datetime_example.py
@@ -1,10 +1,10 @@
 import datetime as dt
 from datetime import datetime
 
-today = dt.datetime.today()
+# today = dt.datetime.today() # It is preffered that you don't use the today method but instead use now.  # nqa
 now = datetime.now()
 utc_now = datetime.utcnow()
 
-print(today)
+# print(today) # It is preffered that you don't use the today method but instead use now.  # nqa
 print(now)
 print(utc_now)

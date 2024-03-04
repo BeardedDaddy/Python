58e588232bdc8be41166758f93eb2456fd8dc748
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-04 05:31:02 -0500

    Relearning how to print out a f-string using variables.

--- UdemyClasses/Django3FullStackWebsite/pythonrefresher.py
+++ UdemyClasses/Django3FullStackWebsite/pythonrefresher.py
@@ -2,3 +2,8 @@ age = 50
 
 name = 'Grevy'
 
+# print(f'Hello my name is {name} and I am {age} years old.')
+
+sentence = 'Hello my name is {} and I am {} years old.'.format(name, age)  # noqa
+
+print(sentence)

f1cea342960e983e61898e38b0ce2b7dfac48e2d
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-06 05:40:55 -0500

    Going over python functions.

--- UdemyClasses/Django3FullStackWebsite/pythonrefresher.py
+++ UdemyClasses/Django3FullStackWebsite/pythonrefresher.py
@@ -1,15 +1,23 @@
-age = 50
+# age = 50
 
-name = 'Grevy'
+# name = 'Grevy'
 
 # sentence = 'Hello my name is {} and I am {} years old.'.format(name, age)  # noqa
 
 # print(sentence)
 # todayIsCold = False
 
-print(f'Hello my name is {name} and I am {age} years old.')
+# print(f'Hello my name is {name} and I am {age} years old.')
 
-if age >= 18:
-    print('You are of legal age.')
-else:
-    print('You are not old enough.')
+# if age >= 18:
+#     print('You are of legal age.')
+# else:
+#     print('You are not old enough.')
+
+
+def hello(name="Mia", age=13):
+    return "Good morning {} you are {} years old.".format (name, age)  # noqa
+
+x = hello()
+
+print(x)

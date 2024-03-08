7fa377591ec73a9f1e36354fedd55fd6c2fa8d4d
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-07 21:58:44 -0500

    Added the smiling and sad face emojis to lines 13 and 15.

--- Python_Projects/TalkToBaymax.py
+++ Python_Projects/TalkToBaymax.py
@@ -10,8 +10,8 @@ print(" My name is Baymax. ")
 time.sleep(3)
 day = input(" Are you having a wonderful day? (y/n) ")
 if day == "y":
-    print("That's great to hear " + name + "!")
+    print("That's great to hear " + name + "!", ("\U0001F600"))
 elif day == "n":
-    print("I'm sorry to hear that " + name + "")
+    print("I'm sorry to hear that " + name + "", ("\U0001F614"))
 else:
     print(" Please enter an y for yes or n for no " + name + "?")

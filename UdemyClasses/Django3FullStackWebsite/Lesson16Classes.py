6674d0ca842e6296bab40804dec2796b3d683885
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-08 06:41:12 -0500

    Refreshing on classes.

---
+++ UdemyClasses/Django3FullStackWebsite/Lesson16Classes.py
@@ -0,0 +1,10 @@
+class Dog():
+    """This is an class called Dog. It has a method called bark.
+    """
+    def bark(self):
+        """This is a function that prints WOOF! to the console.
+        """
+        print("WOOF!")
+
+mydog = Dog()  # Here we are creating an instance of the Dog class and assigning it to the variable mydog. Which simply means to assign the class to the variable.
+mydog.bark()  # Here we are calling the bark method of the mydog instance of the Dog class. Which simply means to call the method of the class.

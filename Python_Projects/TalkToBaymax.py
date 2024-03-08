920ae25ae5c7c6929ee2e0b644be497f4249967e
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-08 00:26:32 -0500

    Cleanup and add docstring.

--- Python_Projects/TalkToBaymax.py
+++ Python_Projects/TalkToBaymax.py
@@ -1,9 +1,8 @@
+"""Import time modules to use in the program."""
 import time
-import emoji
 
 # Get the user's name
 name = input("Please enter your name? ")
-# print(" Hello, " + name + " it is nice to meet you.", emoji.emojize(":grinning_face_with_big_eyes:"))
 print(" Hello, " + name + " it is nice to meet you.", ("\U0001F603"))
 time.sleep(3)
 print(" My name is Baymax. ")

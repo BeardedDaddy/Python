fd1d08ad40fd3c35c557e93556b4cb5ffb7b8488
Grevy <77815094+BeardedDaddy@users.noreply.github.com>
2024-03-18 06:31:16 -0400

    Added lines 18 thru line 21.

--- Python_Projects/password_generator-project/generator/views.py
+++ Python_Projects/password_generator-project/generator/views.py
@@ -13,10 +13,17 @@ def password(request):
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('~!@#$%^&*()_+=-'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
 
-    Password = ''
+    length = int(request.GET.get('length', 12))
+
+    password = ''
     for x in range(length):
-        Password += random.choice(characters)
+        password += random.choice(characters)
 
-    return render(request, 'generator/password.html', {'password': Password})  # noqa
+    return render(request, 'generator/password.html', {'password': password})  # noqa

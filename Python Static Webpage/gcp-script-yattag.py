827e3d4952174894dc163b02377ab0f2bd5566d5
Mr.Grevy <grevyjr@gmail.com>
2024-06-17 06:45:30 -0400

    Created a html python script using yattag.

---
+++ Python Static Webpage/gcp-script-yattag.py
@@ -0,0 +1,20 @@
+from yattag import Doc
+
+doc, tag, text = Doc().tagtext()
+
+doc.asis('<!DOCTYPE html>')
+
+with tag("html"):
+    with tag('head'):
+        with tag('title'):
+            text('Blade comes in theaters November 7 2025')
+        with tag('body'):
+            with tag('p'):
+                text('Blade was the first Marvel Movie that premiered back in August 21 1998')
+                
+result = doc.getvalue()
+
+from yattag import indent
+
+with open('index.html', 'w') as file:
+    file.writelines(indent(result))
